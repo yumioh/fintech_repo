fd0c1e56f7428874e43247aa8351adf1aa87c440
yumi oh <13yooms@gmail.com>
2023-09-11 20:07:23 +0900

    2023911_실습복스

---
+++ pratice_self/비동기실습01.py
@@ -0,0 +1,21 @@
+import requests, json
+from bs4 import BeautifulSoup
+
+url = "https://jsonplaceholder.typicode.com/photos"
+response = requests.get(url)
+
+all_list = []
+
+for value in response.json():
+    comment_list = {
+        "id": value['id'], 
+        "title": value['title'],
+        "comment":[]
+    }
+
+    comment_url = "https://jsonplaceholder.typicode.com/comments?postId=1"#+ str(id)
+    comment_res = requests.get(comment_url)
+    for comment in comment_res.json():
+        comment_list['comment'].append(comment['body'])
+    all_list.append(comment_list) 
+print(all_list)
